995ef44a0d5f02b3851099dc97c3c3480eaa1708
miroslavmatocha <matocham254@gmail.com>
2021-12-06 14:21:49 +0100

    rollback main

--- custom_solution/main.py
+++ custom_solution/main.py
@@ -7,17 +7,17 @@ if __name__ == "__main__":
     graph_path = '../test_cases/aalborg_storcenter.json'
     graph = Graph(graph_path)
 
-    alg = ExitDijkstra("toilets", 10, graph, EdgeCostFunction())
-    result = alg.search()
-    for i in result: 
-        print(i)
-    
+    # get all shortest paths
+    alg = BulkSearchAlgorithm(graph.get_nodes_with_people(), graph, EdgeCostFunction())
 
-    constraints = Constraint(graph.get_edge("crossroad_2", "crossroad_1"), 1, 10)
+    shortest_paths = alg.search()
 
-    alg = ConstrainedExitDijkstra("toilets", 10, graph, EdgeCostFunction(), [constraints])
-    result = alg.search()
-    for i in result: 
-        print(i)
+    for p in shortest_paths:
+        print(p)
 
+    # find collisions between shortest paths
+    collisions = identify_all_collisions(shortest_paths, graph)
+    collisions = merge_collision(collisions)
 
+    # create all possible constraints for one of the collisions
+    constraints_for_first_collision = collisions[0].get_all_constraints()
