4fc67e5c40ff051e3eb855ca39f551b153aba358
Annedrew <1196244890@qq.com>
2021-12-07 12:50:17 +0100

    xml_to_json: modify the order of coordinate(lon, lat), delete the way has the same start and end node, build edge between neighboring node.

--- custom_solution/xml_to_json.py
+++ custom_solution/xml_to_json.py
@@ -1,9 +1,9 @@
 import xml.etree.ElementTree as et
 import json
+import copy
 
 
 def extract_xml(filename):
-    a = []
     originalTree = et.parse(filename)
     originalNode = originalTree.getroot()
     root = et.Element(originalNode.tag)
@@ -18,31 +18,52 @@ def extract_xml(filename):
             for key, value in child.attrib.items():
                 if key == 'id':
                     wayChild.set(key, value)
+            j = 0
+            i = 0
             for child1 in child:
-                if child1.tag == 'nd':
+                j = j + 1
+                if child1.tag == 'nd' and i != 0 and j != len(child.findall('nd')):
                     ndChild = et.SubElement(wayChild, 'nd')
                     for key, value in child1.attrib.items():
                         if key == 'ref':
                             ndChild.set(key, value)
-                            a.append(value)
+                        dupe = copy.deepcopy(child1)
+                        wayChild.append(dupe)
+                if child1.tag == 'nd' and i == 0 or j == len(child.findall('nd')):
+                    ndChild = et.SubElement(wayChild, 'nd')
+                    for key, value in child1.attrib.items():
+                        if key == 'ref':
+                            ndChild.set(key, value)
+                    i = 1
     tree = et.ElementTree(root)
     tree.write('temp.xml')
     nodeTree = et.parse('temp.xml')
     nodeRoot = nodeTree.getroot()
-    root2 = et.Element(nodeRoot.tag)
+    a = []
     for node in nodeRoot.findall('node'):
-        if node.attrib['id'] not in a:
-            nodeRoot.remove(node)
+        b = []
         for way in nodeRoot.findall('way'):
+            j = 0
+            for nd in way.findall('nd'):
+                if j == 0:
+                    b.append(nd.attrib['ref'])
+                if j == len(way.findall('nd')) - 1:
+                    b.append(nd.attrib['ref'])
+                j = j + 1
+            if b[len(b) - 1] == b[len(b) - 2]:
+                nodeRoot.remove(way)
             for nd in way.findall('nd'):
                 if nd.attrib['ref'] == node.attrib['id']:
                     nd.attrib.update(node.attrib)
+                    a.append(nd.attrib['ref'])
+        if node.attrib['id'] not in a:
+            nodeRoot.remove(node)
     nodeTree.write('temp.xml')
     return 'temp.xml'
 
 
 def xml_to_json(filename):
-    i = 0
+    i = 1
     list = []
     list2 = []
     building = {"type": "FeatureCollection"}
@@ -52,26 +73,27 @@ def xml_to_json(filename):
         coor = []
         if nodeChild.tag == 'node':
             singlearray = {"type": "Feature", "properties": {"name": nodeChild.attrib['id']},
-                           "geometry": {"type": "Point", "coordinates": [float(nodeChild.attrib['lat']),
-                                                                         float(nodeChild.attrib['lon'])]}}
+                           "geometry": {"type": "Point", "coordinates": [float(nodeChild.attrib['lon']),
+                                                                         float(nodeChild.attrib['lat'])]}}
             list.append(singlearray)
         if nodeChild.tag == 'way':
+            coor = []
             for ndChild in nodeChild:
-                coor.append([float(ndChild.attrib['lat']), float(ndChild.attrib['lon'])])
-                if i == 0:
-                    firstNd = ndChild.attrib['id']
-                if i == len(nodeChild) - 1:
-                    lastNd = ndChild.attrib['id']
+                coor.append([float(ndChild.attrib['lon']), float(ndChild.attrib['lat'])])
+                if i % 2 != 0:
+                    fm = ndChild.attrib['id']
+                if i % 2 == 0:
+                    t = ndChild.attrib['id']
+                    single = {"type": 'Feature', "properties": {"from": fm, "to": t},
+                          "geometry": {"type": "LineString",
+                                       "coordinates": coor}}
+                    list2.append(single)
+                    coor = []
                 i = i + 1
-            i = 0
-            single = {"type": 'Feature', "properties": {"from": firstNd, "to": lastNd},
-                      "geometry": {"type": "LineString",
-                                   "coordinates": coor}}
-            list2.append(single)
     combined = list + list2
     building.update({"features": combined})
     with open("map.json", 'w') as f:
         f.write(json.dumps(building, indent=4))
     return
 
-xml_to_json(extract_xml('map(3).osm'))
+xml_to_json(extract_xml('zoo.osm'))
